dcb11f42cff4f1e40af93cacaa97fa1bb588d825
Emre Kursun <emre.kursun@student.ehb.be>
2020-05-16 17:57:54 +0200

    Thumbnail + link van video toegevoegd + css

--- DevelopmentIV_Werkstuk_Emre_Kursun/js/script.js
+++ DevelopmentIV_Werkstuk_Emre_Kursun/js/script.js
@@ -1,12 +1,13 @@
 $(function () {
     console.log('linked');
 
+    // Geef de class 'active' bij het drukken van een knop om te filteren.
     $('button').click(function (e) {
         $(this).toggleClass('active');
         e.preventDefault();
     });
 
-
+    // Hier nemen we de data van de json file.
     fetch("json/entries.json").then(function (resp) {
             return resp.json();
         })
@@ -20,14 +21,19 @@ $(function () {
                         let entries = $(`<p class='data'>` + JSON.stringify(data.items) + `<strong><br> </strong></p>`).appendTo(div);
                         JSON.stringify(div);
             */
-            var totalMessages = Object.keys(data.items).length;
+            // Hier nemen we de lengte van de json file.
+            var totalItems = Object.keys(data.items).length;
 
-            for (var i = 0; i < totalMessages; i++) {
+            // Hier loopen we eigenlijk binnen 'totalItems' die de array items bevat.
+            for (var i = 0; i < totalItems; i++) {
+                // Hier maken we een lege div
                 let div = $("<div>", {
 
                 });
+                // Hier fetchen we de category
                 let category = $(`<p class='category'>` + (data.items[i].category) + `<strong><br> </strong></p>`).appendTo(div);
 
+                // Geef div een bepaalde class op basis van de category
                 if (data.items[i].category == 'familie') {
                     $(div).addClass('familie');
                 }
@@ -36,23 +42,33 @@ $(function () {
                     $(div).addClass('volwassenen');
                 }
 
-
+                // genre filtering -NOT DONE-
                 $('.genre').click(function () {
                     $('.genre').removeClass('active').addClass('inactive');
                     $(this).removeClass('inactive').addClass('active');
                 });
 
                 //  let created_by = $(`<p class='data'>` + JSON.stringify(data.items[i]["created-by"]) + `<strong><br> </strong></p>`).appendTo(div);
+
+                // Thumbnail
+                let thumbnail = $(`<img class="thumbnail" src=` + data.items[i]["link-to-video"].metadata.thumbnail_url + `><br>`).appendTo(div); 
+                // Video
+                let video = $(`<a href=` + data.items[i]["link-to-video"].url + `><img class="play" src="images/shapes-and-symbols.png"></a><br><hr>` ).appendTo(div);
+                // Naam
                 let name = $(`<p class='name'>` + (data.items[i]["name"]) + `</p>`).appendTo(div);
-                let created_on = $(`<p class='createdOn'>` + (data.items[i]["created-on"]) + `<strong><br> </strong></p>`).appendTo(div);
+                // let created_on = $(`<p class='createdOn'>` + (data.items[i]["created-on"]) + `<strong><br> </strong></p>`).appendTo(div);
                 let excerpt = $(`<p class='excerpt'>` + (data.items[i]["excerpt"]) + `<strong><br> </strong></p>`).appendTo(div);
-                let key_takeaways = $(`<p class='keyTakeaways'>` + (data.items[i]["key-takeaways"]) + `</p>`).appendTo(div);
+                // keys
+                let key_takeaways = $(`<p class='keyTakeaways'>` + (data.items[i]["key-takeaways"]) + `</p>`).appendTo(div);   
+                
+                // Hier append ik alle data in een div met als ID '#container'
                 $('#container').append(div);
 
-                // VOLWASSENEN BUTTON
+                // VOLWASSENEN BUTTON FILTERING
                 $('.volwassenenBtn').click(function (e) {
                     e.preventDefault();
 
+                    // als enkel deze actief is
                     if ($('.volwassenenBtn').hasClass('active')) {
                         $(".volwassenen").show();
                         $(".familie").hide();
@@ -61,19 +77,23 @@ $(function () {
                         $(".volwassenen").hide();
                     }
 
+                    // Als beide actief zijn
                     if ($('.volwassenenBtn').hasClass('active') && $('.familieBtn').hasClass('active')) {
                         $(".volwassenen").show();
                         $(".familie").show();
                     }
                     
+                    // Als beide niet actief zijn
                     if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active')) {
                         $(".volwassenen").show();
                         $(".familie").show();
                     }
 
-                    // FAMILY BUTTON
+                    // FAMILY BUTTON FILTERING
                     $('.familieBtn').click(function(e){
                         e.preventDefault();
+
+                         // als enkel deze actief is
                         if ($('.familieBtn').hasClass('active')) {
                             $(".familie").show();
                             $(".volwassenen").hide();
@@ -82,20 +102,19 @@ $(function () {
                             $(".familie").hide();
                         }
 
+                        // Als beide actief zijn
                         if ($('.volwassenenBtn').hasClass('active') && $('.familieBtn').hasClass('active')) {
                             $(".volwassenen").show();
                             $(".familie").show();
                         }
 
+                        // Als beide niet actief zijn
                         if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active')) {
                             $(".volwassenen").show();
                             $(".familie").show();
                         }
-    
-
                     });   
                 });
-
             }
         });
 })
