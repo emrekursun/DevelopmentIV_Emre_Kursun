541ede3d0afb98ffa1e419342ba12e158d11fc32
Emre Kursun <emre.kursun@student.ehb.be>
2020-05-19 16:12:28 +0200

    Genre filtering toegepast

--- DevelopmentIV_Werkstuk_Emre_Kursun/js/script.js
+++ DevelopmentIV_Werkstuk_Emre_Kursun/js/script.js
@@ -42,12 +42,7 @@ $(function () {
                     $(div).addClass('volwassenen');
                 }
 
-                // genre filtering -NOT DONE-
-                $('.genre').click(function () {
-                    $('.genre').removeClass('active').addClass('inactive');
-                    $(this).removeClass('inactive').addClass('active');
-                });
-
+            
                 //  let created_by = $(`<p class='data'>` + JSON.stringify(data.items[i]["created-by"]) + `<strong><br> </strong></p>`).appendTo(div);
 
                 // Thumbnail
@@ -60,7 +55,94 @@ $(function () {
                 let excerpt = $(`<p class='excerpt'>` + (data.items[i]["excerpt"]) + `<strong><br> </strong></p>`).appendTo(div);
                 // keys
                 let key_takeaways = $(`<p class='keyTakeaways'>` + (data.items[i]["key-takeaways"]) + `</p>`).appendTo(div);   
+                // Voeg een class toe voor elke specifieke genre
+                let contains = data.items[i]["key-takeaways"];
+                console.log(contains);
+
+                if (contains.indexOf("concert") >= 0) {
+                    $(div).addClass('concert');
+                }
+
+                if (contains.indexOf("Concert") >= 0) {
+                    $(div).addClass('concert');
+                }
+
+                if (contains.indexOf("theater") >= 0) {
+                    $(div).addClass('theater');
+                }
+
+                if (contains.indexOf("Theater") >= 0) {
+                    $(div).addClass('theater');
+                }
+
+                if (contains.indexOf("muziektheater") >= 0) {
+                    $(div).addClass('muziektheater');
+                }
+
+                if (contains.indexOf("Muziektheater") >= 0) {
+                    $(div).addClass('muziektheater');
+                }
+
+                if (contains.indexOf("dans") >= 0) {
+                    $(div).addClass('dans');
+                }
+
+                if (contains.indexOf("Dans") >= 0) {
+                    $(div).addClass('dans');
+                }
                 
+                if (contains.indexOf("figurentheater") >= 0) {
+                    $(div).addClass('figurentheater');
+                }
+
+                if (contains.indexOf("Figurentheater") >= 0) {
+                    $(div).addClass('figurentheater');
+                }
+                
+                if (contains.indexOf("multidisciplinair") >= 0) {
+                    $(div).addClass('multidisciplinair');
+                }
+
+                if (contains.indexOf("Multidisciplinair") >= 0) {
+                    $(div).addClass('multidisciplinair');
+                }
+                
+                if (contains.indexOf("opera") >= 0) {
+                    $(div).addClass('opera');
+                }
+
+                if (contains.indexOf("Opera") >= 0) {
+                    $(div).addClass('opera');
+                }
+                
+                if (contains.indexOf("literatuur") >= 0) {
+                    $(div).addClass('literatuur');
+                }
+
+                if (contains.indexOf("Literatuur") >= 0) {
+                    $(div).addClass('literatuur');
+                }
+
+                if (contains.indexOf("comedy") >= 0) {
+                    $(div).addClass('comedy');
+                }
+
+                if (contains.indexOf("Comedy") >= 0) {
+                    $(div).addClass('comedy');
+                }
+
+                if (contains.indexOf("circus") >= 0) {
+                    $(div).addClass('circus');
+                }
+
+                if (contains.indexOf("Circus") >= 0) {
+                    $(div).addClass('circus');
+                }
+                
+                
+                
+
+
                 // Hier append ik alle data in een div met als ID '#container'
                 $('#container').append(div);
 
@@ -82,12 +164,13 @@ $(function () {
                         $(".volwassenen").show();
                         $(".familie").show();
                     }
-                    
+                   
                     // Als beide niet actief zijn
                     if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active')) {
                         $(".volwassenen").show();
                         $(".familie").show();
                     }
+                });
 
                     // FAMILY BUTTON FILTERING
                     $('.familieBtn').click(function(e){
@@ -113,8 +196,275 @@ $(function () {
                             $(".volwassenen").show();
                             $(".familie").show();
                         }
-                    });   
-                });
+                    });  
+
+
+                    // GENRE FILTERING CODE
+                    
+                    $('.dansBtn').click(function(e){
+                        e.preventDefault();
+
+                         // als enkel deze actief is
+                        if ($('.dansBtn').hasClass('active')) {
+                           
+                            $(".concert").hide();
+                            $(".theater").hide();
+                            $(".multidisciplinair").hide();
+                            $(".literatuur").hide();
+                            $(".comedy").hide();
+                            $(".opera").hide();
+                            $(".circus").hide();
+                           
+                            $(".dans").show();
+                        }
+                        
+
+                        // Als beide actief zijn
+                        if ($('.dansBtn').hasClass('active') && $('.theaterBtn').hasClass('active')) {
+                            $(".dans").show();
+                            $(".theater").show();
+                            
+                        }
+
+                        // Als beide niet actief zijn
+                        if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active') && !$('.dansBtn').hasClass('active')) {
+                          
+                        }
+                    }); 
+                    
+                    $('.theaterBtn').click(function(e){
+                        e.preventDefault();
+
+                         // als enkel deze actief is
+                        if ($('.theaterBtn').hasClass('active')) {
+                           
+                            $(".concert").hide();
+                            $(".dans").hide();
+                            $(".multidisciplinair").hide();
+                            $(".literatuur").hide();
+                            $(".comedy").hide();
+                            $(".opera").hide();
+                            $(".circus").hide();
+                           
+                            $(".theater").show();
+                        }
+                        
+
+                        // Als beide actief zijn
+                        if ($('.theaterBtn').hasClass('active') && $('.dansBtn').hasClass('active')) {
+                            $(".dans").show();
+                            $(".theater").show();
+                           
+                        }
+
+                        // Als beide niet actief zijn
+                        if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active') && !$('.theaterBtn').hasClass('active')) {
+                            
+                        }
+                    });
+
+                    $('.concertBtn').click(function(e){
+                        e.preventDefault();
+
+                         // als enkel deze actief is
+                        if ($('.concertBtn').hasClass('active')) {
+                           
+                            $(".dans").hide();
+                            $(".theater").hide();
+                            $(".multidisciplinair").hide();
+                            $(".literatuur").hide();
+                            $(".comedy").hide();
+                            $(".opera").hide();
+                            $(".circus").hide();
+                           
+                            $(".concert").show();
+                        }
+                        
+
+                        // Als beide actief zijn
+                        if ($('.concertBtn').hasClass('active') && $('.theaterBtn').hasClass('active') && $('.dansBtn').hasClass('active')) {
+                            $(".dans").show();
+                            $(".theater").show();
+                            $(".concert").show();
+                        }
+
+                        // Als beide niet actief zijn
+                        if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active') && !$('.concertBtn').hasClass('active')) {
+                            
+                        }
+                    }); 
+
+                    $('.multiBtn').click(function(e){
+                        e.preventDefault();
+
+                         // als enkel deze actief is
+                        if ($('.multiBtn').hasClass('active')) {
+                           
+                            $(".concert").hide();
+                            $(".theater").hide();
+                            $(".dans").hide();
+                            $(".literatuur").hide();
+                            $(".comedy").hide();
+                            $(".opera").hide();
+                            $(".circus").hide();
+                           
+                            $(".multdisciplinair").show();
+                        }
+                        
+
+                        // Als beide actief zijn
+                        if ($('.multiBtn').hasClass('active') && $('.concertBtn').hasClass('active') && $('.theaterBtn').hasClass('active') && $('.dansBtn').hasClass('active')) {
+                            $(".dans").show();
+                            $(".theater").show();
+                            $(".concert").show();
+                            $(".multidisciplinair").show();
+                        }
+
+
+                        // Als beide niet actief zijn
+                        if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active') && !$('.multiBtn').hasClass('active')) {
+                           
+                        }
+                    }); 
+                    
+                    $('.literatuurBtn').click(function(e){
+                        e.preventDefault();
+
+                         // als enkel deze actief is
+                        if ($('.literatuurBtn').hasClass('active')) {
+                           
+                            $(".concert").hide();
+                            $(".theater").hide();
+                            $(".dans").hide();
+                            $(".multidisciplinair").hide();
+                            $(".comedy").hide();
+                            $(".opera").hide();
+                            $(".circus").hide();
+                           
+                            $(".literatuur").show();
+                        }
+                        
+
+                        // Als beide actief zijn
+                        if ($('.literatuurBtn').hasClass('active') && $('.multiBtn').hasClass('active') && $('.concertBtn').hasClass('active') && $('.theaterBtn').hasClass('active')  && $('.dansBtn').hasClass('active')) {
+                            $(".dans").show();
+                            $(".theater").show();
+                            $(".concert").show();
+                            $(".multidisciplinair").show();
+                            $(".literatuur").show();
+                        }
+
+                        // Als beide niet actief zijn
+                        if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active') && !$('.literatuurBtn').hasClass('active')) {
+                           
+                        }
+                    }); 
+
+                    $('.comedyBtn').click(function(e){
+                        e.preventDefault();
+
+                         // als enkel deze actief is
+                        if ($('.comedyBtn').hasClass('active')) {
+                           
+                            $(".concert").hide();
+                            $(".theater").hide();
+                            $(".dans").hide();
+                            $(".literatuur").hide();
+                            $(".multidisciplinair").hide();
+                            $(".opera").hide();
+                            $(".circus").hide();
+                           
+                            $(".comedy").show();
+                        }
+                        
+
+                        // Als beide actief zijn
+                        if ($('.comedyBtn').hasClass('active') && $('.literatuurBtn').hasClass('active') && $('.multiBtn').hasClass('active') && $('.concertBtn').hasClass('active') && $('.theaterBtn').hasClass('active')  && $('.dansBtn').hasClass('active')) {
+                            $(".dans").show();
+                            $(".theater").show();
+                            $(".concert").show();
+                            $(".multidisciplinair").show();
+                            $(".literatuur").show();
+                            $(".comedy").show();
+                        }
+
+                        // Als beide niet actief zijn
+                        if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active') && !$('.comedyBtn').hasClass('active')) {
+                           
+                        }
+                    }); 
+
+                    $('.operaBtn').click(function(e){
+                        e.preventDefault();
+
+                         // als enkel deze actief is
+                        if ($('.operaBtn').hasClass('active')) {
+                           
+                            $(".concert").hide();
+                            $(".theater").hide();
+                            $(".dans").hide();
+                            $(".literatuur").hide();
+                            $(".comedy").hide();
+                            $(".multidisciplinair").hide();
+                            $(".circus").hide();
+                           
+                            $(".opera").show();
+                        }
+                        
+
+                        // Als beide actief zijn
+                        if ($('.operaBtn').hasClass('active') &&  $('.comedyBtn').hasClass('active') && $('.literatuurBtn').hasClass('active') && $('.multiBtn').hasClass('active') && $('.concertBtn').hasClass('active') && $('.theaterBtn').hasClass('active')  && $('.dansBtn').hasClass('active')) {
+                            $(".dans").show();
+                            $(".theater").show();
+                            $(".concert").show();
+                            $(".multidisciplinair").show();
+                            $(".literatuur").show();
+                            $(".comedy").show();
+                            $(".opera").show();
+                        }
+                        // Als beide niet actief zijn
+                        if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active') && !$('.operaBtn').hasClass('active')) {
+                           
+                        } 
+                    });
+                    
+                    $('.circusBtn').click(function(e){
+                        e.preventDefault();
+
+                         // als enkel deze actief is
+                        if ($('.circusBtn').hasClass('active')) {
+                             
+                            $(".concert").hide();
+                            $(".theater").hide();
+                            $(".dans").hide();
+                            $(".literatuur").hide();
+                            $(".comedy").hide();
+                            $(".opera").hide();
+                            $(".multidisciplinair").hide();
+                           
+                            $(".circus").show();
+                        }
+                        
+
+                        // Als beide actief zijn
+                        if ($('.circusBtn').hasClass('active') && $('.operaBtn').hasClass('active') &&  $('.comedyBtn').hasClass('active') && $('.literatuurBtn').hasClass('active') && $('.multiBtn').hasClass('active') && $('.concertBtn').hasClass('active') && $('.theaterBtn').hasClass('active')  && $('.dansBtn').hasClass('active')) {
+                            $(".dans").show();
+                            $(".theater").show();
+                            $(".concert").show();
+                            $(".multidisciplinair").show();
+                            $(".literatuur").show();
+                            $(".comedy").show();
+                            $(".opera").show();
+                            $(".circus").show();
+                        }
+
+                        // Als beide niet actief zijn
+                        if (!$('.volwassenenBtn').hasClass('active') && !$('.familieBtn').hasClass('active') && !$('.circusBtn').hasClass('active')) {
+                           
+                        }
+                    }); 
+
+
             }
         });
 })
